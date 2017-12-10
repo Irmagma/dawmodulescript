547fbc1e71214cc1a02ddaafe2b6c2ddc08c3f65
Irmagma <31437226+Irmagma@users.noreply.github.com>
2017-12-10 22:40:22 +0000

    dawmodule.sh

---
+++ dawmodule.sh
@@ -0,0 +1,21 @@
+echo "***************************"
+echo " "
+echo "Greetings"
+echo " "
+echo "***************************"
+setxkbmap fi
+sudo apt-get update
+sudo apt-get -y install git tree puppet
+
+git clone https://github.com/Irmagma/daw.git
+
+
+sudo puppet apply --modulepath ~/daw/puppet/modules/ -e 'class {"daw":}'
+
+cd
+
+echo "***************************"
+echo " "
+echo "Done"
+echo " "
+echo "***************************"
